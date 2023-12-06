92f477eb3167735a55b17994a031ef8d977295e3
Amit kumar singh <notgetin18@gmail.com>
2023-12-06 14:03:44 +0530

    redux with session storage

--- src/app/coins/[...slug]/page.tsx
+++ src/app/coins/[...slug]/page.tsx
@@ -1,24 +1,17 @@
 'use client'
-import { api, metalPrice } from '@/api/DashboardServices';
-import { funcForDecrypt } from '@/components/helperFunctions';
-import { setGoldData, setSilverData } from '@/redux/metalSlice';
-import { setMetalPrice } from '@/redux/shopSlice';
+import { api } from '@/api/DashboardServices';
+import { ParseFloat, funcForDecrypt } from '@/components/helperFunctions';
 import { RootState } from '@/redux/store';
-import React, { useCallback, useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 
-const Test = ({ params }: any) => {
+const page = ({ params }: any) => {
     const id = params.slug;
     const [productsDetailById, setProductDetailById] = useState<any>();
     const [quantity, setQuantity] = useState<number>(1);
     const goldData = useSelector((state: RootState) => state.gold);
     const silverData = useSelector((state: RootState) => state.silver);
-    console.log('gold data', goldData)
-    console.log('gold data', goldData.totalPrice)
-    const metalPricePerGram = useSelector((state: RootState) => state.shop.metalPrice);
-    console.log('metalPricePerGram', metalPricePerGram)
-
-
+    // console.log('gold data', goldData)
 
     const getProductById = async () => {
         try {
@@ -40,7 +33,7 @@ const Test = ({ params }: any) => {
 
 
 
-    // console.log("productsDetailById", productsDetailById);
+    console.log("productsDetailById", productsDetailById);
     const increaseQty = () => {
         if (quantity <= 9) {
             setQuantity(quantity + 1);
@@ -58,25 +51,42 @@ const Test = ({ params }: any) => {
 
     return (
         <div className='text-white'>
-            <div className='grid grid-cols-3'>
+            <div className='flex gap-x-12 w-full'>
                 <div>
-                    <div>Image swiper  ===== {productsDetailById.itemtype === "GOLD" ? goldData.totalPrice : silverData.totalPrice}</div>
+                    <div>Image swiper , metal price{productsDetailById.iteamtype === "GOLD" ? goldData.totalPrice : silverData.totalPrice}</div>
                     <div className=''>
                         <div>Add to cart</div>
                         <div>Buy Now</div>
                     </div>
                 </div>
-                <div className='ml-6'>
-                    <div>{productsDetailById.name}</div>
-                    <div>Total Price {+productsDetailById.weight * quantity * (productsDetailById.itemtype === "GOLD" ? goldData.totalPrice : silverData.totalPrice)}</div>
+                <div className='flex items-center justify-between'>
+                    <div className=''>
+                        <div>{productsDetailById.name}</div>
+                        <div >Total Price <span className='text-yellow-500'>₹{ParseFloat(+productsDetailById.weight * quantity * (productsDetailById.iteamtype === "GOLD" ? goldData.totalPrice : silverData.totalPrice), 2)} </span><span className='text-yellow-500'>+3% GST</span></div>
+                        <div>Making Charge ₹{productsDetailById.makingcharges}</div>
+                        <div className='py-2'>
+                            <div>CHECK PINCODE AVAILABILITY</div>
+                            <input type="number"
+                                value={''}
+                            />
+                        </div>
+                    </div>
+                </div>
+                <div className='flex  items-center rounded-lg bg-slate-500'>
+                    <div onClick={decreaseQty} className={styles.p1}>-</div>
+                    <div className='m-2 p-2'>{quantity}</div>
+                    <div onClick={increaseQty} className={styles.p2}>+</div>
                 </div>
             </div>
+
         </div>
     );
 };
+const styles = {
+    p1: 'm-2 p-2 font-bold text-lg text-red-500 cursor-pointer rounded-lg bg-slate-300',
+    p2: 'm-2 p-2 font-bold text-lg text-green-500 cursor-pointer rounded-sm bg-slate-300',
 
-
-
-export default Test;
+}
+export default page;
 
 
--- src/redux/store.tsx
+++ src/redux/store.tsx
@@ -1,21 +1,34 @@
 // store.ts
 import { combineReducers } from 'redux';
 import { configureStore } from '@reduxjs/toolkit';
-import {silverReducer, goldReducer}  from './metalSlice'; 
+import { silverReducer, goldReducer } from './metalSlice';
 import shopSlice from './shopSlice';
 import couponSlice from './couponSlice';
 import timerReducer from './timeSlice';
+import storage from 'redux-persist/lib/storage';
+import { persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER, } from 'redux-persist';
 
-export const store = configureStore({
-    reducer: {
-        gold: goldReducer, 
-        silver: silverReducer, 
-        shop: shopSlice,
-        coupon: couponSlice,
-        time: timerReducer,
-        // ... other reducers
-    },
+const persistConfig = {
+    key: 'root',
+    storage,
+};
+
+const rootReducer = combineReducers({ gold: goldReducer, silver: silverReducer, shop: shopSlice, coupon: couponSlice, time: timerReducer });
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+export default configureStore({
+    reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            serializableCheck: {
+                ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+            },
+        }),
 });
+
+const store = configureStore({
+    reducer: persistedReducer
+})
 export type RootState = ReturnType<typeof store.getState>;
 // export type AppDispatch = typeof store.dispatch;
 
