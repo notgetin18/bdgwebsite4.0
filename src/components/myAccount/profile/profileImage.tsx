46670724d75e4b1a003182972627ed536603f5f0
Raghav Singh <raghvender3@gmail.com>
2024-01-02 15:57:27 +0530

    faq page

--- src/components/myAccount/profile/profileImage.tsx
+++ src/components/myAccount/profile/profileImage.tsx
@@ -1,82 +1,96 @@
-import { AesDecrypt } from '@/components/helperFunctions';
-import { AppDispatch } from '@/redux/store';
-import { fetchUserDetails, selectUser } from '@/redux/userDetailsSlice';
-import axios from 'axios';
-import React, { useEffect, useRef, useState } from 'react'
-import { useDispatch, useSelector } from 'react-redux';
-import Image from 'next/image';
-import { FaEdit } from 'react-icons/fa';
+import { AesDecrypt } from "@/components/helperFunctions";
+import { AppDispatch } from "@/redux/store";
+import { fetchUserDetails, selectUser } from "@/redux/userDetailsSlice";
+import axios from "axios";
+import React, { useEffect, useRef, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import Image from "next/image";
+import { FaEdit } from "react-icons/fa";
 
 const ProfileImage = () => {
-    const user = useSelector(selectUser);
-    const dispatch: AppDispatch = useDispatch();
-    const fileInputRef = useRef(null);
-    const [isSubmitting, setIsSubmitting] = useState(false)
-    const [isLoading, setIsLoading] = useState(false);
+  const user = useSelector(selectUser);
+  const dispatch: AppDispatch = useDispatch();
+  const fileInputRef = useRef(null);
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
-    const handleProfileImageChange = async (event: any) => {
-        if (!isSubmitting) {
-            setIsSubmitting(true);
-            setIsLoading(true);
-            event.stopPropagation();
-            const formData = new FormData();
-            formData.append("profile_image", event.target.files[0]);
-            formData.append('payload', 'CD46F0B542BF044C3F5CEC4AFA6AC27A')
+  const handleProfileImageChange = async (event: any) => {
+    if (!isSubmitting) {
+      setIsSubmitting(true);
+      setIsLoading(true);
+      event.stopPropagation();
+      const formData = new FormData();
+      formData.append("profile_image", event.target.files[0]);
+      formData.append("payload", "CD46F0B542BF044C3F5CEC4AFA6AC27A");
 
-            try {
-                const token = localStorage.getItem('token')
-                const configHeaders = { headers: { authorization: `Bearer ${token}`, 'Content-Type': 'multipart/form-data' } }
-                const response = await axios.post(`${process.env.baseUrl}/user/profile/image`, formData, configHeaders
-                )
-                const decryptedData = AesDecrypt(response.data.payload)
-                const finalResult = JSON.parse(decryptedData)
-                if (finalResult.status) {
-                    setIsLoading(false);
-                } else {
-                    alert("Error while uploading Profile Image")
-                }
-            } catch (error) {
-                console.error(error);
-            } finally {
-                setIsLoading(false);
-                setIsSubmitting(false);
-            }
+      try {
+        const token = localStorage.getItem("token");
+        const configHeaders = {
+          headers: {
+            authorization: `Bearer ${token}`,
+            "Content-Type": "multipart/form-data",
+          },
+        };
+        const response = await axios.post(
+          `${process.env.baseUrl}/user/profile/image`,
+          formData,
+          configHeaders
+        );
+        const decryptedData = AesDecrypt(response.data.payload);
+        const finalResult = JSON.parse(decryptedData);
+        if (finalResult.status) {
+          setIsLoading(false);
+        } else {
+          alert("Error while uploading Profile Image");
         }
+      } catch (error) {
+        console.error(error);
+      } finally {
+        setIsLoading(false);
+        setIsSubmitting(false);
+      }
+    }
+  };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      dispatch(fetchUserDetails());
     };
 
-    useEffect(() => {
-        const fetchData = async () => {
-            dispatch(fetchUserDetails());
-        };
+    fetchData();
+  }, [dispatch, handleProfileImageChange]);
+  return (
+    <div className="coins_background rounded-t-lg w-full flex flex-col items-center justify-center relative">
+      <Image
+        src={user.data.profile_image}
+        alt="profile image"
+        width={100}
+        height={100}
+        className="my-4 rounded-full h-24 w-24 flex items-center justify-center"
+      />
 
-        fetchData();
-    }, [dispatch, handleProfileImageChange]);
-    return (
-        <div className="coins_background rounded-t-lg w-full flex flex-col items-center justify-center relative">
-            <Image src={user.data.profile_image} alt="profile image" width={100} height={100} className="mt-4 rounded-full h-24 w-24 flex items-center justify-center" />
-
-            {/* Edit icon */}
-            <div className="absolute top-2 right-2 text-gray-400 hover:text-gray-200 focus:outline-none">
-                <div className="relative">
-                    <input
-                        type="file"
-                        id="file"
-                        ref={fileInputRef}
-                        name="profileImage"
-                        className="sr-only"
-                        onChange={handleProfileImageChange}
-                        accept="image/*"
-                    />
-                    <label
-                        htmlFor="file"
-                        className="absolute top-2 right-2 text-gray-400 hover:text-gray-200 focus:outline-none"
-                    >
-                        <FaEdit className="cursor-pointer" color={'yellow'} size={20} />
-                    </label>
-                </div>
-            </div>
+      {/* Edit icon */}
+      <div className="absolute top-2 right-2 text-gray-400 hover:text-gray-200 focus:outline-none">
+        <div className="relative">
+          <input
+            type="file"
+            id="file"
+            ref={fileInputRef}
+            name="profileImage"
+            className="sr-only"
+            onChange={handleProfileImageChange}
+            accept="image/*"
+          />
+          <label
+            htmlFor="file"
+            className="absolute top-2 right-2 text-themeBlueLight focus:outline-none"
+          >
+            <FaEdit className="cursor-pointer" size={20} />
+          </label>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default ProfileImage
+export default ProfileImage;
--- src/components/myAccount/profile/profileInfo.tsx
+++ src/components/myAccount/profile/profileInfo.tsx
@@ -1,107 +1,122 @@
-import { AppDispatch } from '@/redux/store';
-import { selectUser } from '@/redux/userDetailsSlice';
-import React, { useState } from 'react'
-import { useDispatch, useSelector } from 'react-redux';
-import { VscVerifiedFilled } from 'react-icons/vsc';
-import { MdScheduleSend } from 'react-icons/md';
-import Swal from 'sweetalert2';
-import { AesDecrypt } from '@/components/helperFunctions';
-
+import { AppDispatch } from "@/redux/store";
+import { selectUser } from "@/redux/userDetailsSlice";
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { VscVerifiedFilled } from "react-icons/vsc";
+import { MdScheduleSend } from "react-icons/md";
+import Swal from "sweetalert2";
+import { AesDecrypt } from "@/components/helperFunctions";
 
 const ProfileInfo = ({ onEditDetailsClick }: any) => {
-    const user = useSelector(selectUser);
-    const [isSubmitting, setIsSubmitting] = useState(false)
-    const [isLoading, setIsLoading] = useState(false);
+  const user = useSelector(selectUser);
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
-
-
-    const verifyEmail = () => {
-
-        if (!isSubmitting) {
-            setIsSubmitting(true);
-            const token = localStorage.getItem("token");
-            const configHeaders = {
-                headers: {
-                    authorization: `Bearer ${token}`,
-                    "Content-Type": "application/json",
-                },
-            };
-            fetch(`${process.env.baseUrl}/user/validate/email`, configHeaders)
-                .then((response) => response.json())
-                .then(async (response) => {
-                    const decryptedData = await AesDecrypt(response.payload)
-                    const finalResult = JSON.parse(decryptedData);
-                    Swal.fire({
-                        position: 'center',
-                        icon: 'success',
-                        title: finalResult.message,
-                        showConfirmButton: false,
-                        timer: 3000
-                    })
-                })
-                .catch(async (errorInVerifyEmail) => {
-                    const decryptedData = await AesDecrypt(errorInVerifyEmail.payload)
-                    const finalResult = JSON.parse(decryptedData);
-                    Swal.fire({
-                        position: 'center',
-                        icon: 'error',
-                        title: finalResult.message,
-                        showConfirmButton: false,
-                        timer: 3000
-                    })
-                }).finally(() => {
-                    setIsLoading(false);
-                    setIsSubmitting(false);
-                })
-        }
+  const verifyEmail = () => {
+    if (!isSubmitting) {
+      setIsSubmitting(true);
+      const token = localStorage.getItem("token");
+      const configHeaders = {
+        headers: {
+          authorization: `Bearer ${token}`,
+          "Content-Type": "application/json",
+        },
+      };
+      fetch(`${process.env.baseUrl}/user/validate/email`, configHeaders)
+        .then((response) => response.json())
+        .then(async (response) => {
+          const decryptedData = await AesDecrypt(response.payload);
+          const finalResult = JSON.parse(decryptedData);
+          Swal.fire({
+            position: "center",
+            icon: "success",
+            title: finalResult.message,
+            showConfirmButton: false,
+            timer: 3000,
+          });
+        })
+        .catch(async (errorInVerifyEmail) => {
+          const decryptedData = await AesDecrypt(errorInVerifyEmail.payload);
+          const finalResult = JSON.parse(decryptedData);
+          Swal.fire({
+            position: "center",
+            icon: "error",
+            title: finalResult.message,
+            showConfirmButton: false,
+            timer: 3000,
+          });
+        })
+        .finally(() => {
+          setIsLoading(false);
+          setIsSubmitting(false);
+        });
     }
+  };
 
-
-    return (
-        <div>
-            <div className="w-full p-4 text-white flex flex-col">
-                <div className="mb-2 flex justify-between rounded-b-lg">
-                    <span className="font-bold">Name</span>
-                    <span>{user?.data?.name}</span>
-                </div>
-                <hr className="border-gray-500 my-1" />
-                <div className="mb-2 flex justify-between">
-                    <span className="font-bold">Mobile Number</span>
-                    <span>{user?.data?.mobile_number}</span>
-                </div>
-                <hr className="border-gray-500 my-1" />
-                <div className="mb-2 flex justify-between">
-                    <span className="font-bold">Date of Birth</span>
-                    <span>{new Date(user?.data?.dateOfBirth).toLocaleDateString("en-IN", {
-                        day: "2-digit",
-                        month: "short",
-                        year: "numeric",
-                    })}</span>
-                </div>
-                <hr className="border-gray-500 my-1" />
-                <div className="mb-2 flex justify-between">
-                    <span className="font-bold">Email ID</span>
-                    <span className='flex items-center'>{user?.data?.email}<span className='pl-2'>{user?.data?.isEmailVerified ? <VscVerifiedFilled className="cursor-pointer" color={'green'} size={24} /> : <MdScheduleSend onClick={verifyEmail} className="cursor-pointer" color={'yellow'} size={24} />}</span></span>
-                </div>
-                <hr className="border-gray-500 my-1" />
-                <div className="mb-2 flex justify-between">
-                    <span className="font-bold">Gender</span>
-                    <span>{user?.data?.gender?.toUpperCase()}</span>
-                </div>
-                <hr className="border-gray-500 my-1" />
-                {/* <div className="flex justify-between">
+  return (
+    <div>
+      <div className="w-full p-4 text-white flex flex-col text-sm sm:text-base">
+        <div className="mb-2 flex justify-between rounded-b-lg">
+          <span className="font-bold">Name</span>
+          <span>{user?.data?.name}</span>
+        </div>
+        <hr className="border-gray-500 my-1" />
+        <div className="mb-2 flex justify-between">
+          <span className="font-bold">Mobile Number</span>
+          <span>{user?.data?.mobile_number}</span>
+        </div>
+        <hr className="border-gray-500 my-1" />
+        <div className="mb-2 flex justify-between">
+          <span className="font-bold">Date of Birth</span>
+          <span>
+            {new Date(user?.data?.dateOfBirth).toLocaleDateString("en-IN", {
+              day: "2-digit",
+              month: "short",
+              year: "numeric",
+            })}
+          </span>
+        </div>
+        <hr className="border-gray-500 my-1" />
+        <div className="mb-2 grid grid-cols-2">
+          <span className="font-bold">Email ID</span>
+          <span className="sm:flex items-center break-words justify-end">
+            {user?.data?.email}
+            <span className="pl-2 inline-block">
+              {user?.data?.isEmailVerified ? (
+                <VscVerifiedFilled
+                  className="cursor-pointer"
+                  color={"green"}
+                  size={24}
+                />
+              ) : (
+                <MdScheduleSend
+                  onClick={verifyEmail}
+                  className="cursor-pointer"
+                  color={"yellow"}
+                  size={24}
+                />
+              )}
+            </span>
+          </span>
+        </div>
+        <hr className="border-gray-500 my-1" />
+        <div className="mb-2 flex justify-between">
+          <span className="font-bold">Gender</span>
+          <span>{user?.data?.gender?.toUpperCase()}</span>
+        </div>
+        <hr className="border-gray-500 my-1" />
+        {/* <div className="flex justify-between">
                     <span className="font-bold">GST No.</span>
                     <span>{user.data.gst_number || 'N/A'}</span>
                 </div> */}
-            </div>
-            <div className='border-2 border-yellow-400 rounded mb-4 text-center'>
-                <button className='text-yellow-400 py-2 ' onClick={onEditDetailsClick}>
-                    Edit Details
-                </button>
-            </div>
-        </div>
-    )
-}
+      </div>
+      <div className="bg-themeBlue rounded text-center mx-3">
+        <button className="font-semibold py-2 " onClick={onEditDetailsClick}>
+          Edit Details
+        </button>
+      </div>
+    </div>
+  );
+};
 
-export default ProfileInfo
-
+export default ProfileInfo;
